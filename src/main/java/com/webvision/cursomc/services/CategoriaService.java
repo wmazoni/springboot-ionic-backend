84b5b338b8b08b33078e072a37f66ad686319541
Wellington Andrade <wmazoni@protonmail.com>
2020-07-20 17:42:55 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/webvision/cursomc/services/CategoriaService.java
+++ src/main/java/com/webvision/cursomc/services/CategoriaService.java
@@ -6,16 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.webvision.cursomc.domain.Categoria;
+import com.webvision.cursomc.exceptions.ObjectNotFoundException;
 import com.webvision.cursomc.repositories.CategoriaRepository;
+
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
