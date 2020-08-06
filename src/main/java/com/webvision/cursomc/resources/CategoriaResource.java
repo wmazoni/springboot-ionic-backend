f868eac85b2aa7a20e939f9934cdb04a01af975e
Wellington Andrade <wmazoni@protonmail.com>
2020-08-05 21:01:51 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/webvision/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/webvision/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.webvision.cursomc.resources;
 
 import com.webvision.cursomc.dto.CategoriaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -54,4 +55,14 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(listDTO);
 	}
 
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue = "0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value="direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+	}
 }
