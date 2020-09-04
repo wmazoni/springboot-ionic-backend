d65c3fd029cf029c23ea7bd6743602cd1fd81633
Wellington Andrade <wmazoni@protonmail.com>
2020-09-03 22:16:40 -0300

    Inserindo Pedido

--- src/main/java/com/webvision/cursomc/resources/PedidoResource.java
+++ src/main/java/com/webvision/cursomc/resources/PedidoResource.java
@@ -2,13 +2,14 @@ package com.webvision.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.webvision.cursomc.domain.Pedido;
 import com.webvision.cursomc.services.PedidoService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value="/pedidos")
@@ -20,9 +21,15 @@ public class PedidoResource {
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
-		
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/webvision/cursomc/services/PedidoService.java
+++ src/main/java/com/webvision/cursomc/services/PedidoService.java
@@ -1,13 +1,19 @@
 package com.webvision.cursomc.services;
 
-import java.util.Optional;
-
+import com.webvision.cursomc.domain.ItemPedido;
+import com.webvision.cursomc.domain.PagamentoComBoleto;
+import com.webvision.cursomc.domain.Pedido;
+import com.webvision.cursomc.domain.enums.EstadoPagamento;
+import com.webvision.cursomc.repositories.ItemPedidoRepository;
+import com.webvision.cursomc.repositories.PagamentoRepository;
+import com.webvision.cursomc.repositories.PedidoRepository;
+import com.webvision.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.webvision.cursomc.domain.Pedido;
-import com.webvision.cursomc.services.exceptions.ObjectNotFoundException;
-import com.webvision.cursomc.repositories.PedidoRepository;
+import java.util.Date;
+import java.util.Optional;
 
 
 @Service
@@ -16,9 +22,42 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado((EstadoPagamento.PENDENTE));
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
