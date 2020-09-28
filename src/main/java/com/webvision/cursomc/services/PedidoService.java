db78b0cdb89d75d7a86573099918a89ad3d47325
Wellington Andrade <wmazoni@protonmail.com>
2020-09-27 22:17:48 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/webvision/cursomc/services/PedidoService.java
+++ src/main/java/com/webvision/cursomc/services/PedidoService.java
@@ -1,5 +1,17 @@
 package com.webvision.cursomc.services;
 
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.webvision.cursomc.domain.Categoria;
+import com.webvision.cursomc.domain.Cliente;
 import com.webvision.cursomc.domain.ItemPedido;
 import com.webvision.cursomc.domain.PagamentoComBoleto;
 import com.webvision.cursomc.domain.Pedido;
@@ -7,13 +19,9 @@ import com.webvision.cursomc.domain.enums.EstadoPagamento;
 import com.webvision.cursomc.repositories.ItemPedidoRepository;
 import com.webvision.cursomc.repositories.PagamentoRepository;
 import com.webvision.cursomc.repositories.PedidoRepository;
+import com.webvision.cursomc.security.UserSS;
+import com.webvision.cursomc.services.exceptions.AuthorizationException;
 import com.webvision.cursomc.services.exceptions.ObjectNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Date;
-import java.util.Optional;
 
 
 @Service
@@ -68,5 +76,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
