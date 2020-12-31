de14924bb7ee83a3962595be8187a7a0c6a80509
Wellington Andrade <wmazoni@protonmail.com>
2020-12-31 12:12:18 -0300

    Salvando primeiro commit no S3.

--- src/main/java/com/webvision/cursomc/CursomcApplication.java
+++ src/main/java/com/webvision/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.webvision.cursomc;
 
+import com.webvision.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -15,7 +18,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\temp\\beach.jpg");
 		
 	}
 }
